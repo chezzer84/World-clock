f615d17a52defc42977a63ad5a02bb8d89a4cc7f
chezzer84 <115329113+chezzer84@users.noreply.github.com>
2023-11-05 14:53:06 +0800

    Added styling removed Singapore

--- Javascript/index.js
+++ Javascript/index.js
@@ -26,20 +26,6 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
-
-  //Singapore
-
-  let singaporeElement = document.querySelector("#singapore");
-  if (singaporeElement) {
-    let singaporeDateElement = singaporeElement.querySelector(".date");
-    let singaporeTimeElement = singaporeElement.querySelector(".time");
-    let singaporeTime = moment().tz("Singapore");
-
-    singaporeDateElement.innerHTML = singaporeTime.format("MMMM Do yyyy");
-    singaporeTimeElement.innerHTML = singaporeTime.format(
-      "h:mm:ss [<small>]A[</small>]"
-    );
-  }
 }
 
 function updateCity(event) {
